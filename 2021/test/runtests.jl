8976ec8eb7860a02b341b7168d40b8750b5354b0
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2023-11-16 13:58:32 +0100

    add some 2021 days

--- 2021/test/runtests.jl
+++ 2021/test/runtests.jl
@@ -12,6 +12,25 @@ end
     @test AdventOfCode21.Day03.day03() == [4139586, 1800151]
 end
 
+@testset "Day 4" begin
+    @test AdventOfCode21.Day04.day04() == [72770, 13912]
+end
+
+@testset "Day 5" begin
+    @test AdventOfCode21.Day05.day05() == [8350, 19374]
+end
+
+@testset "Day 6" begin
+    @test AdventOfCode21.Day06.day06() == [353274, 1609314870967]
+end
+
+@testset "Day 7" begin
+    @test AdventOfCode21.Day07.day07() == [349357, 96708205]
+end
+
+@testset "Day 8" begin
+    @test AdventOfCode21.Day08.day08() == [272, 1007675]
+end
 
 @testset "Day 18" begin
     @test AdventOfCode21.Day18.day18() == [4323, 4749]

740997881ef862ea106e3edf2ecb7d0ab484a36a
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2023-12-02 10:09:19 +0100

    day02

--- 2023/test/runtests.jl
+++ 2023/test/runtests.jl
@@ -3,3 +3,7 @@ using AdventOfCode.AdventOfCode23
 @testset "Day 01" begin
     @test AdventOfCode23.Day01.day01() == [54450, 54265]
 end
+@testset "Day 02" begin
+    @test AdventOfCode23.Day02.day02() == [2447, 56322]
+end
+
